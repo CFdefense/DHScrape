84cf4444786c8eaba80d31f335bcd49d0c06a189
CFdefense <152815652+CFdefense@users.noreply.github.com>
2024-09-03 15:31:20 -0400

    found way to parse and get json using subprocess curl. Now can manipulate

--- DiningScraper/scrape.py
+++ DiningScraper/scrape.py
@@ -1,4 +1,15 @@
+import subprocess
+import json
 class Scraper:
     @staticmethod
     def getMenu(link):
-       # need to call api
+        command = [
+            'curl', 'https://api.dineoncampus.com/v1/sites/todays_menu?site_id=64b18f3bc625af0685e3c5eb'
+        ]
+        # Execute the 'ls -l' command and capture its output
+        curl_request = subprocess.run(command, capture_output=True, text=True)
+
+        # log as json
+        json_data = json.loads(curl_request.stdout)
+
+        #todays food note is chicken tinga
